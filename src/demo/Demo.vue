fe4c7f4a436975952add6660951521ccd295be13
Cristhian Daza <cristian.d.b@hotmail.com>
2025-09-05 21:45:45 -0500

    chore(demo): update version to 1.1.1 in Demo.vue

--- src/demo/Demo.vue
+++ src/demo/Demo.vue
@@ -15,7 +15,7 @@ const TvButton = defineAsyncComponent(/* webpackChunkName: "TvButton" */() => im
     npmInstall="@todovue/tv-button"
     sourceLink="https://github.com/TODOvue/tv-button"
     urlClone="https://github.com/TODOvue/tv-button.git"
-    version="1.1.0"
+    version="1.1.1"
   ></tv-demo>
 </template>
 
